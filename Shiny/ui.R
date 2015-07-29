76e325300bd879bda9798f82ba4f7b864bd2b7a4
Daniel P. Martin <dpmartin42@gmail.com>
2015-07-29 16:41:55 -0400

    change ui

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,57 +1,60 @@
 library(shiny)
 library(leaflet)
 
-shinyUI(navbarPage("Food Findr",
- tabPanel("App",
-          sidebarLayout(
-            sidebarPanel(
-              
-              helpText("Your guide to local, healthy dining options in Boston, MA."),
-              
-              tags$hr(),
-              
-              textInput("address", label = "Enter your address", 
-                        value = "e.g., 50 Milk Street"),
-              
-              tags$hr(),
-              
-              sliderInput("distance", label = "Max distance (in mi.)", min = 0.1, 
-                          max = 3.0, value = 0.5),
-              
-              tags$hr(),
-              
-              checkboxGroupInput("price", 
-                                 label = "Price preference", 
-                                 choices = list("$" = "$", 
-                                                "$$" = "$$",
-                                                "$$$" = "$$$",
-                                                "$$$$" = "$$$$",
-                                                "$$$$$" = "$$$$$"),
-                                 selected = c("$", "$$", "$$$", "$$$$", "$$$$$")),
-              
-              tags$hr(),
-              
-              checkboxGroupInput("restrictions", 
-                                 label = "Dietary restrictions", 
-                                 choices = list("Vegetarian-friendly" = "vegetarian-friendly", 
-                                                "Vegan" = "vegan",
-                                                "Gluten-free" = "gluten-free"),
-                                 selected = c()),
-              
-              tags$hr(),
-              
-              submitButton("Update View")
-              
-            ),
-            
-            mainPanel(tabsetPanel(
-              tabPanel("Map", leafletOutput("mymap", width = 800, height = 600)), 
-              tabPanel("Table", dataTableOutput("restaurants"))
-            )
-            )
-            
-          )),
- 
+shinyUI(navbarPage(theme = "bootstrap.min.css", "Food Findr",
+ tabPanel("Interactive Map",
+          
+    div(class = "outer",
+        
+        tags$head(
+          
+          includeCSS("styles.css")
+          
+          ),
+        
+        leafletOutput("mymap", width = "80%", height = "100%"),
+        
+        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                      draggable = FALSE, top = 50, left = "auto", right = 0,
+                      width = "20%", height = "100%",
+                      
+                      br(),
+                      
+                      textInput("address", label = "Enter your address", 
+                                value = "e.g., 50 Milk Street"),
+                            
+                      tags$hr(),
+                            
+                      sliderInput("distance", label = "Max distance (in mi.)", min = 0.1, 
+                                  max = 3.0, value = 0.5),
+                            
+                      tags$hr(),
+                            
+                      checkboxGroupInput("price", 
+                                         label = "Price preference", 
+                                         choices = list("$" = "$", 
+                                                        "$$" = "$$",
+                                                              "$$$" = "$$$",
+                                                              "$$$$" = "$$$$",
+                                                              "$$$$$" = "$$$$$"),
+                                               selected = c("$", "$$", "$$$", "$$$$", "$$$$$")),
+                      
+                      tags$hr(),
+                      
+                      checkboxGroupInput("restrictions", 
+                                         label = "Dietary restrictions", 
+                                         choices = list("Vegetarian-friendly" = "vegetarian-friendly", 
+                                                        "Vegan" = "vegan",
+                                                        "Gluten-free" = "gluten-free"),
+                                         selected = c()),
+                            
+                      tags$hr(),
+                            
+                      submitButton("Update View")
+              )
+          )
+ ),
+ tabPanel("Restaurant Explorer", dataTableOutput("restaurants")),
  tabPanel("Slides",
           tags$iframe(src = "https://docs.google.com/presentation/embed?id=1QemHAqv_vbBhpe09lYs_7l-f0FlS8u2g2YXWrdvxcio&amp;start=false&amp;loop=false&amp;",
                       frameborder = "0",
@@ -59,7 +62,8 @@ shinyUI(navbarPage("Food Findr",
                       height = "569",
                       allowfullscreen = TRUE,
                       mozallowfullscreen = TRUE,
-                      webkitallowfullscreen = TRUE))
+                      webkitallowfullscreen = TRUE)
+          )
   
 ))
 
